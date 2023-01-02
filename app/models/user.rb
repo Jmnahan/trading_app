e654f51576781cf8358377ab5589cc657df066c2
knrtaven <52444839+knrtaven@users.noreply.github.com>
2023-01-02 15:58:46 +0800

    added mail confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,20 @@
 class User < ApplicationRecord
+  before_validation :auto_approve_admin, on: :create
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   enum role: [:buyer, :broker, :admin]
+  enum status: {pending: 0, approved: 1}
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
     self.role ||= :buyer 
+  end
+
+  def auto_approve_admin
+    self.status = admin? ? :approved : :pending
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+user = User.new email: 'user1@email.com', password: 'password'
+admin = User.new email: 'admin1@email.com', password: 'password', role: :admin
+
+user.skip_confirmation!
+admin.skip_confirmation!
+
+user.save
+admin.save
