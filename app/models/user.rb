6aeeaf7c1d0b0e87e354edc3c36dbe60dbfd6ff1
root <root@DESKTOP-09EMUN4.localdomain>
2023-01-03 20:22:14 +0800

    added user status

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,22 @@
 class User < ApplicationRecord
+  after_initialize :set_default_role, :if => :new_record?
+  before_validation :set_status, on: :create
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  enum role: [:buyer, :broker, :admin]
-  after_initialize :set_default_role, :if => :new_record?
+  enum role: [:buyer, :admin]
+  enum status: [:pending, :approved]
+
+  private
 
   def set_default_role
     self.role ||= :buyer 
+  end
+
+  def set_status
+    self.status ||= :pending
   end
 end
